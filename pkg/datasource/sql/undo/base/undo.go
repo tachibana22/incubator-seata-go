fd52490fd8746debb80305d371c5d90242737f9a
yunfan24 <yunfan24@outlook.com>
2026-05-15 01:13:44 +0800

    fix: fix ci lint error

--- pkg/datasource/sql/undo/base/undo.go
+++ pkg/datasource/sql/undo/base/undo.go
@@ -632,7 +632,7 @@ func (m *BaseUndoLogManager) getSerializer(undoLogContext map[string]string) (se
 	if undoLogContext == nil {
 		return
 	}
-	serializer, _ = undoLogContext[serializerKey]
+	serializer = undoLogContext[serializerKey]
 	return
 }
 
